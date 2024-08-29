c12abbd0158cbffe0fae1c3ff4d9c4c40c6883cb
Eason-CS <easonh2027@student.cis.edu.hk>
2024-08-29 17:59:25 +0800

    my first commit

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello! Please type in you name");
+        int age = -1;
+        while (age < 0) {
+            System.out.println("enter a valid number.");
+            try {
+                age = Integer.parseInt(scan.nextLine());
+            }
+            catch(NumberFormatException e) {
+            }
+
+        int dating  = age / 2 + 7;
+    System.out.println(age + " year olds should date someone who is at least " + dating + " years old.");
+    }
     }
 }
+
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hi, please enter your age");
+        int age = Integer.parseInt(scan.nextLine());
+        int pages = 100 - age;
+        System.out.println(age + " year olds should read at least " + pages + " pages before giving up on a book.");
     }
 }
+
